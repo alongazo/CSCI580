20815473883119fcd00d514a8f326926139d85c2
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2017-11-23 01:21:44 -0800

    Remove unused files.

--- Render/TriangleCollection.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RENDER_TRIANGLECOLLECTION_H
-#define _RENDER_TRIANGLECOLLECTION_H
-
-#include <memory>
-
-class TriangleCollection;
-typedef std::shared_ptr<TriangleCollection> TriangleCollectionPtr;
-
-#endif
